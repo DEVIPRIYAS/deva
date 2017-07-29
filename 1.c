4a99551acbd8c79299db95c60f8b1540041a03f9
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-07-29 04:13:40 -0700

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("Enter a number:");
+scanf("%lf",&number);
+if number<=0.0
+{
+if(number==0.0)
+printf("you entered a positive number.");
+}
+else
+printf("you entered a positive number.");
+return 0;
+}
